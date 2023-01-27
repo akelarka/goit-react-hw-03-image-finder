dee4b5358a73b5ad89ccb6e3ff284bf7b2e9f4ff
D. Akelar <108392983+akelarka@users.noreply.github.com>
2023-01-28 01:30:47 +0200

    add own style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
-import { ButtonLoad } from './Button.styled';
+import { ButtonLoad, ButtonWrapper } from './Button.styled';
 
 const Button = ({ text, loadMore }) => {
     return (
-        <ButtonLoad onClick={loadMore} type="button">
-        {text}
-        </ButtonLoad>
+        <ButtonWrapper>
+            <ButtonLoad onClick={loadMore} type="button">
+            {text}
+            </ButtonLoad>
+        </ButtonWrapper>
     )
 };
 
